f29ca052f9f381fa84978641ace9225baad4bf4d
JihoLeec <Jiholee.py@gmail.com>
2026-09-06 16:36:56 +0900

    fix: fix crawler github link

--- src/data/toys.ts
+++ src/data/toys.ts
@@ -11,6 +11,6 @@ export const toys: ToyProject[] = [
 		title: 'K-MOOC / KOCW 강의 목록 크롤러',
 		summary: '공개된 수천 개의 강의 목록을 수집해 액셀에 자동으로 정리하는 크롤러',
 		tags: ['Python'],
-		href: 'https://github.com/JihoLeec',
+		href: 'https://github.com/JihoLeec/k-edu-course-crawler',
 	},
 ];
