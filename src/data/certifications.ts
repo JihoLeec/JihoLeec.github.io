c03ad879527587ca9fab8004574b7c3509513bf8
JihoLeec <Jiholee.py@gmail.com>
2026-09-26 17:27:40 +0900

    feat: add in-progress certifications toggle

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -6,23 +6,27 @@ export type CertificationItem = {
 
 export const certifications: CertificationItem[] = [
 	{
-		name: '정보처리기사 필기 합격', 
+		name: '정보처리기사 필기 합격',
 		date: '2026.03.11',
 	},
 	{
 		name: '빅데이터분석기사 필기 합격',
 		date: '2026.09.23',
 	},
-	// {
-	// 	name: 'SQLD',
-	// 	date: '2026.12',
-	// },
-	// {
-	// 	name: '한국사능력검정시험 n급',
-	// 	date: '2026.12',
-	// },
-	// {
-	// 	name: 'TOEIC',
-	// 	date: '615',
-	// },
+];
+
+/** 준비 중인 자격증 */
+export const certificationsPrep: CertificationItem[] = [
+	{
+		name: 'SQLD',
+		date: '2026.11.14 응시 예정',
+	},
+	{
+		name: '한국사능력검정시험 1급',
+		date: '2026.10.17 응시 예정',
+	},
+	{
+		name: 'TOEIC',
+		date: '2026.09.29 발표 예정',
+	},
 ];
